a0f47e4d84f66208692b0264661da32b34c24c1f
MJ Abadilla <mjmaix@gmail.com>
2019-04-26 01:31:19 +0800

    basic API mock structure

---
+++ src/core/api/index.ts
@@ -0,0 +1,24 @@
+export const API = {
+  Users: [
+    {
+      uid: '',
+      email: '',
+      avatar: '',
+      name: '',
+    },
+  ],
+  Chat: [
+    {
+      users: [],
+      messages: [
+        {
+          sentBy: '',
+          date: '',
+          content: '',
+          file: '',
+          seenBy: [],
+        },
+      ],
+    },
+  ],
+};
