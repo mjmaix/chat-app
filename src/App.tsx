7ef994b1e4a6f8a12c43865463e46c097e1987bb
MJ Abadilla <mjmaix@gmail.com>
2019-05-20 01:01:49 +0800

    Eject native-base and use StyleProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { StyleProvider } from 'native-base';
 import React, { Component } from 'react';
+import getTheme from '../native-base-theme/components';
+import commonColor from '../native-base-theme/variables/commonColor';
 import { ThemeContextProvider } from './core/themes';
 import { AppRoutes } from './routes/';
 import NavigationService from './routes/NavigationService';
@@ -7,11 +10,13 @@ export default class App extends Component<{}> {
   public render() {
     return (
       <ThemeContextProvider>
-        <AppRoutes
-          ref={navigatorRef => {
-            NavigationService.setTopLevelNavigator(navigatorRef);
-          }}
-        />
+        <StyleProvider style={getTheme(commonColor)}>
+          <AppRoutes
+            ref={navigatorRef => {
+              NavigationService.setTopLevelNavigator(navigatorRef);
+            }}
+          />
+        </StyleProvider>
       </ThemeContextProvider>
     );
   }
