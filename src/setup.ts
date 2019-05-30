22451d7ec7c79bbf47710bec8bea090b9231d6e4
MJ Abadilla <mjmaix@gmail.com>
2019-05-30 19:51:04 +0800

    Configure add Auth

--- src/setup.ts
+++ src/setup.ts
@@ -1,3 +1,7 @@
 import { YellowBox } from 'react-native';
+import Amplify from 'aws-amplify';
+import awsmobile from '../aws-exports';
 
 YellowBox.ignoreWarnings(['Warning: Async']);
+
+Amplify.configure(awsmobile);
