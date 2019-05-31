76d468a8f0dfba8b178f78e8fb6684c10a561f1f
MJ Abadilla <mjmaix@gmail.com>
2019-06-01 02:14:55 +0800

    Install react-native-image-picker

--- android/app/src/main/java/com/mjabadilla/chatapp/MainApplication.java
+++ android/app/src/main/java/com/mjabadilla/chatapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.mjabadilla.chatapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RNAWSCognitoPackage(),
             new RNDeviceInfo(),
             new VectorIconsPackage(),
