42855fa986cbe0093cb67da929e4b91895933717
MJ Abadilla <mjmaix@gmail.com>
2019-06-02 00:30:39 +0800

    group related scren index export

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,14 +3,17 @@ export * from './Auth/PasswordForgotScreen';
 export * from './Auth/PasswordResetScreen';
 export * from './Auth/SignInScreen';
 export * from './Auth/SignUpScreen';
-export * from './Common/AuthLoadingScreen';
 export * from './Auth/ResendSignUpScreen';
+export * from './Auth/NewPasswordScreen';
+
 export * from './Account/PasswordChangeScreen';
 export * from './Account/ProfileScreen';
 export * from './Account/SettingsScreen';
 export * from './Account/VerifyEmailScreen';
+
 export * from './Social/ChatScreen';
 export * from './Social/ContactsScreen';
 export * from './Social/MessagesScreen';
+
+export * from './Common/AuthLoadingScreen';
 export * from './Common/BusyOverlayScreen';
-export * from './Auth/NewPasswordScreen';
