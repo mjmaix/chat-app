0297bbb84de78b57b7276d68d186046462a86b05
MJ Abadilla <mjmaix@gmail.com>
2019-06-01 23:13:07 +0800

    * Install animatable and netinfo

--- android/app/src/main/java/com/mjabadilla/chatapp/MainApplication.java
+++ android/app/src/main/java/com/mjabadilla/chatapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.mjabadilla.chatapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.amazonaws.RNAWSCognitoPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NetInfoPackage(),
             new RNFetchBlobPackage(),
             new ImagePickerPackage(),
             new RNAWSCognitoPackage(),
