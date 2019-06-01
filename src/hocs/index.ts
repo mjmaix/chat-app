e3e1b6f0f46913af399b53e6ef2dca4103b376c6
MJ Abadilla <mjmaix@gmail.com>
2019-06-02 00:54:25 +0800

    group index imports/exports

--- src/hocs/index.ts
+++ src/hocs/index.ts
@@ -2,6 +2,7 @@ import { PreviewAvatarProps } from './../components/Images/PreviewAvatar';
 import { InputProps, ButtonProps } from 'react-native-elements';
 import { FormikProps } from 'formik';
 import { ReactElement } from 'react';
+
 export * from './FormikInputInjector';
 export * from './FormikButtonInjector';
 export * from './withFormikImage';
