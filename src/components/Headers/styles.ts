f01240f8201ce8b172b98136301a1ab488363b0c
MJ Abadilla <mjmaix@gmail.com>
2019-06-04 15:43:37 +0800

    Reuse field validation within file * Margin on left and right for headers

--- src/components/Headers/styles.ts
+++ src/components/Headers/styles.ts
@@ -9,5 +9,6 @@ export const styles = StyleSheet.create({
   headerContainer: {
     alignItems: 'center',
     marginVertical: StyleGuide.gap.big,
+    marginHorizontal: StyleGuide.gap.big,
   },
 });
--- src/core/validators/index.ts
+++ src/core/validators/index.ts
@@ -1,13 +1,21 @@
 import * as Yup from 'yup';
+
+const emailRule = Yup.string()
+  .label('Email')
+  .email()
+  .required();
+
+const phoneRule = Yup.string()
+  .label('Mobile number')
+  .matches(/^[=+\s]*(?:[0-9][=+\s]*){8,}$/, 'Not a valid mobile number')
+  .required();
+
 export const SignUpSchema = Yup.object().shape({
   password: Yup.string()
     .label('Password')
     .min(8)
     .required(),
-  email: Yup.string()
-    .label('Email')
-    .email()
-    .required(),
+  email: emailRule,
   familyName: Yup.string()
     .label('Family name')
     .notRequired()
@@ -16,10 +24,7 @@ export const SignUpSchema = Yup.object().shape({
     .label('Given name')
     .notRequired()
     .ensure(),
-  phoneNumber: Yup.string()
-    .label('Mobile number')
-    .matches(/^[=+\s]*(?:[0-9][=+\s]*){8,}$/, 'Not a valid mobile number')
-    .required(),
+  phoneNumber: phoneRule,
   picture: Yup.string()
     .label('Profile picture')
     .url()
@@ -28,10 +33,7 @@ export const SignUpSchema = Yup.object().shape({
 });
 
 export const UpdateProfileSchema = Yup.object().shape({
-  email: Yup.string()
-    .label('Email')
-    .email()
-    .required(),
+  email: emailRule,
   familyName: Yup.string()
     .label('Family name')
     .notRequired()
@@ -40,10 +42,7 @@ export const UpdateProfileSchema = Yup.object().shape({
     .label('Given name')
     .notRequired()
     .ensure(),
-  phoneNumber: Yup.string()
-    .label('Mobile number')
-    .matches(/^[=+\s]*(?:[0-9][=+\s]*){8,}$/, 'Not a valid mobile number')
-    .required(),
+  phoneNumber: phoneRule,
   picture: Yup.string()
     .label('Profile picture')
     .notRequired()
@@ -54,27 +53,18 @@ export const SignInSchema = Yup.object().shape({
   password: Yup.string()
     .label('Password')
     .required(),
-  email: Yup.string()
-    .label('Email')
-    .email()
-    .required(),
+  email: emailRule,
 });
 
 export const ChallengeSchema = Yup.object().shape({
-  email: Yup.string()
-    .label('Email')
-    .email()
-    .required(),
+  email: emailRule,
   code: Yup.string()
     .label('Code')
     .required('Required'),
 });
 
 export const EmailOnlySchema = Yup.object().shape({
-  email: Yup.string()
-    .label('Email')
-    .email()
-    .required(),
+  email: emailRule,
 });
 
 export const PasswordResetSchema = Yup.object().shape({
@@ -84,10 +74,7 @@ export const PasswordResetSchema = Yup.object().shape({
   code: Yup.string()
     .label('Code')
     .required(),
-  email: Yup.string()
-    .label('Email')
-    .email()
-    .required(),
+  email: emailRule,
 });
 
 export const PasswordChangeSchema = Yup.object().shape({
