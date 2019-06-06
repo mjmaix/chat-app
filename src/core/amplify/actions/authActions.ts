357113c66888902e4f42bf2c6969ad2c26807bfb
MJ Abadilla <mjmaix@gmail.com>
2019-06-07 01:04:32 +0800

    Log authActions method calls to easily find error generating calls

--- src/core/amplify/actions/authActions.ts
+++ src/core/amplify/actions/authActions.ts
@@ -7,6 +7,7 @@ import { Auth } from 'aws-amplify';
 import _ from 'lodash';
 
 import { WrapKnownExceptions } from '../../errors';
+import { logInfo } from '../../reports';
 import {
   ChallengeModel,
   CodeRequiredModel,
@@ -58,7 +59,8 @@ export const handleSignUp = async (data: typeof SignUpModel) => {
 };
 
 export const handleSignIn = async (data: typeof SignInModel) => {
-  const { email, password } = data;
+  logInfo('[START] handleSignIn');
+  const { email, password, phoneNumber } = data;
   const user = await Auth.signIn({
     username: email.toLowerCase(),
     password,
@@ -67,10 +69,12 @@ export const handleSignIn = async (data: typeof SignInModel) => {
 };
 
 export const handleSignOut = async (global = false) => {
+  logInfo('[START] handleSignOut');
   return Auth.signOut({ global }).catch(WrapKnownExceptions);
 };
 
 export const handleUpdateProfile = async (data: typeof ProfileModel) => {
+  logInfo('[START] handleUpdateProfile');
   const { email, familyName, givenName, phoneNumber, picture } = data;
   const user = await Auth.currentUserPoolUser().catch(WrapKnownExceptions);
 
@@ -84,6 +88,7 @@ export const handleUpdateProfile = async (data: typeof ProfileModel) => {
 };
 
 export const handleCheckVerifiedContact = async (opts?: CurrentUserOpts) => {
+  logInfo('[START] handleCheckVerifiedContact');
   const user = await Auth.currentUserPoolUser(opts).catch(WrapKnownExceptions);
   const verif = await Auth.verifiedContact(user).catch(WrapKnownExceptions);
   return verif;
@@ -93,32 +98,38 @@ export const handleVerifyContact = async (
   contact: Contact,
   data: typeof ChallengeModel,
 ) => {
+  logInfo('[START] handleVerifyContact');
   return Auth.verifyCurrentUserAttributeSubmit(contact, data.code).catch(
     WrapKnownExceptions,
   );
 };
 
 export const handleVerifyContactResend = async (contact: Contact) => {
+  logInfo('[START] handleVerifyContactResend');
   return Auth.verifyCurrentUserAttribute(contact).catch(WrapKnownExceptions);
 };
 
 export const handleResend = async (data: typeof EmailModel) => {
+  logInfo('[START] handleResend');
   return Auth.resendSignUp(data.email).catch(WrapKnownExceptions);
 };
 
 export const handleConfirmSignUp = async (data: typeof ChallengeModel) => {
+  logInfo('[START] handleConfirmSignUp');
   return Auth.confirmSignUp(data.email.toLowerCase(), data.code).catch(
     WrapKnownExceptions,
   );
 };
 
 export const handleForgotPassword = async (data: typeof EmailModel) => {
+  logInfo('[START] handleForgotPassword');
   return Auth.forgotPassword(data.email).catch(WrapKnownExceptions);
 };
 
 export const handleForgotPasswordSubmit = async (
   data: typeof PasswordResetModel,
 ) => {
+  logInfo('[START] handleForgotPasswordSubmit');
   await Auth.forgotPasswordSubmit(data.email, data.code, data.password);
   return handleSignOut(true).catch(WrapKnownExceptions);
 };
@@ -126,6 +137,7 @@ export const handleForgotPasswordSubmit = async (
 export const handleChangePasswordSubmit = async (
   data: typeof PasswordChangeModel,
 ) => {
+  logInfo('[START] handleChangePasswordSubmit');
   const currentUser = await Auth.currentUserPoolUser().catch(
     WrapKnownExceptions,
   );
@@ -140,6 +152,7 @@ export const handleCompleteNewPassword = async (
   unAuthUser: ChatCognitoUser,
   data: Partial<typeof SignUpModel> & typeof PasswordRequiredModel,
 ) => {
+  logInfo('[START] handleCompleteNewPassword');
   const { requiredAttributes } = unAuthUser.challengeParam;
   const camelCaseData = _.reduce(
     data,
@@ -156,11 +169,13 @@ export const handleCompleteNewPassword = async (
 };
 
 export const handleSetupMfaTotp = async () => {
+  logInfo('[START] handleSetupMfaTotp');
   const user = await Auth.currentUserPoolUser().catch(WrapKnownExceptions);
   return Auth.setupTOTP(user).catch(WrapKnownExceptions);
 };
 
 export const handleVerifyMfaTotp = async (data: typeof CodeRequiredModel) => {
+  logInfo('[START] handleVerifyMfaTotp');
   const user = await Auth.currentUserPoolUser().catch(WrapKnownExceptions);
   await Auth.verifyTotpToken(user, data.code).catch(WrapKnownExceptions);
 
@@ -172,9 +187,7 @@ export const handleSetMfa = async (mfa: SetPreferredMfa) => {
   return Auth.setPreferredMFA(user, mfa).catch(WrapKnownExceptions);
 };
 
-export const handleGetPreferredMfa: (
-  opts?: GetPreferredMFAOpts,
-) => Promise<MFAChoice> = async opts => {
+  logInfo('[START] handleGetPreferredMfa');
   const user = await Auth.currentUserPoolUser().catch(WrapKnownExceptions);
   return Auth.getPreferredMFA(user, opts).catch(WrapKnownExceptions) as Promise<
     MFAChoice
@@ -185,7 +198,8 @@ export const handleConfirmSignIn = async (
   unAuthUser: ChatCognitoUser,
   data: typeof CodeRequiredModel,
 ) => {
-  const mfaType = unAuthUser.challengeName as Partial<ChallengeMfa>;
+  logInfo('[START] handleConfirmSignIn');
+  const mfaType = unAuthUser.challengeName as Nullable<MfaSignIn>;
   return Auth.confirmSignIn(unAuthUser, data.code, mfaType).catch(
     WrapKnownExceptions,
   );
