8184975e7783473b36ebfa138f372686d5f9f313
MJ Abadilla <mjmaix@gmail.com>
2019-06-09 21:56:58 +0800

    Add more chat message samples

--- src/screens/Social/ChatScreen.tsx
+++ src/screens/Social/ChatScreen.tsx
@@ -30,14 +30,35 @@ class ChatScreen extends Component<Props, State> {
     this.setState({
       messages: [
         {
+          _id: 4,
+          text: 'This is a system message',
+          createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+          system: true,
+          user: {
+            _id: 0,
+          },
+        },
+        {
           _id: 1,
-          text: 'Hello developer',
+          text: 'Chat message',
           createdAt: new Date(),
           user: {
             _id: 2,
             name: 'React Native',
             avatar: 'https://placeimg.com/141/141/people',
           },
+        },
+        {
+          _id: 2,
+          text: 'Image message',
+          createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+          image:
+            'https://camo.githubusercontent.com/d77d7a3eafd85abece54e6297f163930e12f3a87/68747470733a2f2f692e696d6775722e636f6d2f6f553758596b6b2e706e67',
         },
       ],
     });
