588b9c0e6f49a85731debc1138a1d4526cd0c7f0
MJ Abadilla <mjmaix@gmail.com>
2019-06-13 16:05:27 +0800

    * Appsync make username mandatory * Types for subscriptionACtions

--- src/core/actions/subscriptionActions.ts
+++ src/core/actions/subscriptionActions.ts
@@ -1,24 +1,33 @@
+import { ApolloCurrentResult } from 'apollo-client';
 import gql from 'graphql-tag';
 
+import {
+  OnCreateClUserSubscription,
+  OnDeleteClUserSubscription,
+  OnUpdateClUserSubscription,
+} from '../../API';
 import * as subscriptions from '../../graphql/subscriptions';
 import { apolloClient as client } from '../../setup';
 import { logReport as logRecord } from '../reports/index';
 
-type SubscribeNextCallback<T> = (data: T) => void;
+interface DataWrapper<T> extends ApolloCurrentResult<T> {
+  data: T;
+}
+type SubscribeNextCallback<T> = (data: DataWrapper<T>) => void;
 type SubscribeErrorCallback = (error: any) => void;
 
-export const subscribeToCreateClUser = <T>(
+export const subscribeToCreateClUser = <T = OnCreateClUserSubscription>(
   cbNextData: SubscribeNextCallback<T>,
   cbError?: SubscribeErrorCallback,
 ) => {
   const observer = client
-    .subscribe({
+    .subscribe<DataWrapper<T>>({
       query: gql(subscriptions.onCreateClUser),
       fetchPolicy: __DEV__ ? 'no-cache' : undefined,
     })
     .subscribe({
       next: data => {
-        cbNextData(data.data);
+        cbNextData(data);
       },
       error: e => {
         if (cbError) {
@@ -35,18 +44,18 @@ export const subscribeToCreateClUser = <T>(
   return observer;
 };
 
-export const subscribeToUpdateClUser = <T>(
+export const subscribeToUpdateClUser = <T = OnUpdateClUserSubscription>(
   cbNextData: SubscribeNextCallback<T>,
   cbError?: SubscribeErrorCallback,
 ) => {
   const observer = client
-    .subscribe({
+    .subscribe<DataWrapper<T>>({
       query: gql(subscriptions.onUpdateClUser),
       fetchPolicy: __DEV__ ? 'no-cache' : undefined,
     })
     .subscribe({
       next: data => {
-        cbNextData(data.data);
+        cbNextData(data);
       },
       error: e => {
         if (cbError) {
@@ -63,18 +72,18 @@ export const subscribeToUpdateClUser = <T>(
   return observer;
 };
 
-export const subscribeToDeleteClUser = <T>(
+export const subscribeToDeleteClUser = <T = OnDeleteClUserSubscription>(
   cbNextData: SubscribeNextCallback<T>,
   cbError?: SubscribeErrorCallback,
 ) => {
   const observer = client
-    .subscribe({
+    .subscribe<DataWrapper<T>>({
       query: gql(subscriptions.onDeleteClUser),
       fetchPolicy: __DEV__ ? 'no-cache' : undefined,
     })
     .subscribe({
       next: data => {
-        cbNextData(data.data);
+        cbNextData(data);
       },
       error: e => {
         if (cbError) {
